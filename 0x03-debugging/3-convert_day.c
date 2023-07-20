16a69e9196d57a5e3b14cef21aa87dbc598c85fd
NkcubekoM <nkcubekomsengi@gmail.com>
2023-07-20 04:53:56 -0700

    3-convert_day.c

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -10,24 +10,24 @@
 
 int convert_day(int month, int day)
 {
-    switch (month)
-    {
-        case 2:
-            day = 31 + day;
-            break;
-        case 3:
-            day = 59 + day;
-            break;
-        case 4:
-            day = 90 + day;
-            break;
-        case 5:
-            day = 120 + day;
-            break;
-        case 6:
-            day = 151 + day;
-            break;
-        case 7:
+	switch (month)
+	{
+	case 2:
+		day = 31 + day;
+		break;
+	case 3:
+		day = 59 + day;
+		break;
+	case 4:
+		day = 90 + day;
+		break;
+	case 5:
+		day = 120 + day;
+		break;
+	case 6:
+		day = 151 + day;
+		break;
+	case 7:
             day = 181 + day;
 		break;
 	case 8:
