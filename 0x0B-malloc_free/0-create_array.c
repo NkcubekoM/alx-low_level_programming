ebbe2c1155fb4019f2313c54d57d9de17bf609be
NKCUBEKO MSENGI <136090574+NkcubekoM@users.noreply.github.com>
2023-08-07 19:50:03 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,13 +3,9 @@
 
 /**
  * create_array - create array of size size and assign char c
-
  * @size: size of array
-
  * @c: char to assign
-
  * Description: create array of size size and assign char c
-
  * Return: pointer to array, NULL if fail
  */
 
